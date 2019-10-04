15e5cb9d3bd5d0d6af0655187ca22f7432b2948f
MathieuOP <mathieuoliveirapereira@gmail.com>
2019-10-04 23:49:30 +0200

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,3 @@
-// A FAIRE
-// responsive (fait)
-// ajouter la fleche (fait)
-// opacity (fait)
-// régler la view de droite si pas de recherche (fait)
-// aucune ville trouvé (fait)
-// factoriser les fichier (fait)
-
-// valider et changer d'input avec la toucher entrer
-// nettoyer le code
-
-
-
-
 import React from 'react';
 import './style/App.scss';
 import Search from '../containers/Search';
